a5cc7486a6cd3fd23ada0e360cf169b413a40cb0
yipg <around.mr.child@gmail.com>
2020-02-24 20:14:27 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import RandomNumber from "./showNumber";
 
 function App() {
+  const [coords, setCoords] = useState([0, 0]);
+  const [listData, setData] = useState([]);
+
+  useEffect(() => {
+    fetchEmployeeData();
+  }, []);
+
+  const fetchEmployeeData = async () => {
+    const response = await fetch(
+      "http://dummy.restapiexample.com/api/v1/employees"
+    );
+    console.log(response);
+    const dataJson = await response.json();
+    console.log(dataJson);
+    setData(dataJson.data);
+  };
+
+  const doSomething = () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setCoords([position.coords.latitude, position.coords.longitude]);
+    });
+    return;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>伊藤</h1>
+
+      <a href="/link">クリックしてね</a>
+      <h2>
+        {coords[0]}, {coords[1]}
+      </h2>
+      {listData.map(item => {
+        return <div key={item.id}>{item.employee_name}</div>;
+      })}
+      <button onClick={doSomething}>現座地を取得！</button>
+      <button onClick={fetchEmployeeData}>データ取得</button>
+      <h2>ハロー</h2>
+      <RandomNumber />
     </div>
   );
 }
+
+// function Example() {
+//   // Declare a new state variable, which we'll call "count"
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>Click me</button>
+//       <button onClick={() => setCount(count - 1)}>Click me2</button>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/showNumber.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RandomNumber() {
+  return (
+    <div>
+      <h4>1</h4>
+    </div>
+  );
+}
+
+export default RandomNumber;
